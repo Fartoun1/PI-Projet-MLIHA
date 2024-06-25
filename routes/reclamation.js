8a369972368059ddcf3610e98e29cf79ac30ab36
zied <zied.hack@gmail.com>
2024-06-25 21:33:47 +0200

    some change on reclamation

--- routes/reclamation.js
+++ routes/reclamation.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 import multer from "../middlewares/multer-config-local.js";
 
-import {  addOnce, getAll, getOnce, updateOne, deleteOne, updateField }
+import {  addOnce, getAll, getOnce, updateOne, deleteOne, updateField, getReclamation, sendEmail}
 from '../controllers/reclamation.js';
   
 const router = express.Router();
@@ -16,12 +16,22 @@ router
 
 router
   .route('/:idClient')
-  .put(updateOne)
+  .put(multer("image", 5 * 1024 * 1024), 
+    updateOne)
   .get(getOnce);
 
   router
   .route('/:id')
   .delete(deleteOne)
   .patch(updateField);
-  
+
+router
+  .route('/rec/:id')
+  .get(getReclamation);  
+
+router
+  .route('/sendEmail')
+  .post(sendEmail);  
+
+
 export default router;
